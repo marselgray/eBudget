23e6805fb2cd8e44471fcbdc9526d3bc38d24d6d
Marsel Gray <marselgray@gmail.com>
2018-07-30 12:17:08 -0400

    added models

--- budgetapplication/views.py
+++ budgetapplication/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 def project_list(request):
     return render(request, 'budget/project-list.html')
 
 def project_detail(request, project_slug):
-    # fetching the correct project
-    return render(request, 'budget/project-detail.html')
+    # fetch the correct project
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project-detail.html', {'Project': project, 'expense_list': project.expenses.all() })
